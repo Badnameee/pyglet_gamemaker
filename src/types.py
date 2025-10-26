05f632d26051c8c1999d4ab92d4fd3c805ba755c
Badnameee <stevenrrobles13@gmail.com>
2025-10-26 00:35:13 -0500

    Added colors and ButtonStatus

--- src/types.py
+++ src/types.py
@@ -1,12 +1,12 @@
 from enum import Enum
+from typing import Literal
 
 Point2D = tuple[float, float]
 FontInfo = tuple[str | None, int | None]
+ButtonStatus = Literal['Unpressed', 'Hover', 'Pressed']
 
 
 class Color(Enum):
-	WHITE = 255, 255, 255, 255
-	BLACK = 0, 0, 0, 255
 	RED = 255, 0, 0, 255
 	ORANGE = 255, 167, 0, 255
 	YELLOW = 255, 255, 0, 255
@@ -15,3 +15,6 @@ class Color(Enum):
 	BLUE = 0, 0, 255, 255
 	PURPLE = 167, 0, 255, 255
 	MAGENTA = 255, 0, 255, 255
+	WHITE = 255, 255, 255, 255
+	GRAY = 128, 128, 128, 255
+	BLACK = 0, 0, 0, 255
