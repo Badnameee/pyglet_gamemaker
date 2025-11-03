f1c9fb4a347f63fe42ee4b4583df60bf12f616e4
Badnameee <stevenrrobles13@gmail.com>
2025-11-03 14:20:03 -0600

    Added Scene

---
+++ src/scene.py
@@ -0,0 +1,40 @@
+# Built-In Modules
+from abc import ABC, abstractmethod
+
+# Pip Modules
+import pyglet
+from pyglet.window import Window
+from pyglet.event import EventDispatcher
+from pyglet.graphics import Batch
+
+class Scene(ABC, EventDispatcher):
+	"""Holds a scene for the game.
+	
+	Dispatches `on_scene_change` (to window) when program wishes to switch scenes.
+
+	Use kwargs to attach event handlers.
+	"""
+
+	batch: Batch
+
+	def __init__(self, name: str, window: Window, **kwargs) -> None:
+		"""Create a scene.
+
+		Args:
+			name (str): The name of the scene (used to identity scene by name)
+			window (Window): The screen window (for pushing/popping handlers)
+		"""
+		self.name, self.window = name, window
+		
+		self.register_event_type('on_scene_change')
+		# Adds any event handlers passed through kwargs
+		for name in kwargs:
+			self.register_event_type(name)
+		self.push_handlers(**kwargs)
+
+	@abstractmethod
+	def enable(self) -> None:
+		"""Enables this scene. (does not enable rendering, it is always enabled)"""
+	@abstractmethod
+	def disable(self) -> None:
+		"""Disables this scene. (does not disable rendering, it is always enabled)"""
